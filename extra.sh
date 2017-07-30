8a32d2d2276d168998999f0ca4fa9116bd417c34
mathieu <mathieu.hautebas@gmail.com>
2017-07-30 09:38:52 -0700

    added term and Hopper

--- extra.sh
+++ extra.sh
@@ -19,3 +19,10 @@ git clone https://github.com/radare/radare2 && cd radare2
 git clone --recursive https://github.com/xerub/img4lib.git #&& cd img4lib
 make -C img4lib/lzfse 
 make -C img4lib && sudo cp img4lib/img4 /usr/local/bin/img4tool # sorry for name it will conflict with img4
+
+# I use it with qwertyoruiop's serialsh
+git clone https://github.com/matteyeux/term.git
+sudo make -C term install
+
+wget https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.2.10-Linux.deb
+sudo dpkg -i Hopper-v4-4.2.10-Linux.deb
