fbe10fbc9264860454e6a810a65d40e8d2ab5b2b
Mathieu Hautebas <mathieuhautebas@mac-mini.local>
2017-11-26 23:17:56 +0100

    updated script to support macOS

--- extra.sh
+++ extra.sh
@@ -8,34 +8,47 @@ mkdir WD
 cd WD
 
 # crappy code I use to enter in recovery mode without udid
-git clone https://gist.github.com/0c272be43e70ad02012242921dd418be.git ideviceenterrecovery && cd ideviceenterrecovery
-gcc ideviceenterrecovery/ideviceenterrecovery.c -o ideviceenterrecovery/ideviceenterrecovery -limobiledevice
+git clone https://gist.github.com/0c272be43e70ad02012242921dd418be.git ideviceenterrecovery
+gcc ideviceenterrecovery/ideviceenterrecovery.c -o ideviceenterrecovery/ideviceenterrecovery.c -limobiledevice
 sudo cp ideviceenterrecovery/ideviceenterrecovery /usr/local/bin
 
 # Useful if you don't want to dl entire firmware
 git clone https://github.com/matteyeux/libcrippy && cd libcrippy
 ./autogen.sh && sudo make install
+cd ..
 
 git clone https://github.com/matteyeux/libpartialzip && cd libpartialzip
 ./autogen.sh && sudo make install
-sudo ldconfig
+if [[ $(uname) == 'Linux' ]]; then
+	sudo ldconfig
+fi
+cd ..
+
 git clone https://github.com/radare/radare2 && cd radare2
 ./sys/install.sh
+cd ..
 
 git clone https://github.com/matteyeux/iBoot32Patcher
 make -C iBoot32Patcher install
 
-git clone --recursive https://github.com/xerub/img4lib.git 
-make -C img4lib/lzfse 
-make -C img4lib && sudo cp img4lib/img4 /usr/local/bin/img4 
+git clone --recursive https://github.com/xerub/img4tool.git
+make -C img4tool/lzfse
+make -C img4tool && sudo cp img4lib/img4 /usr/local/bin/img4
 
 # I use it with qwertyoruiop's serialsh
 git clone https://github.com/matteyeux/term.git
 sudo make -C term install
 
-sudo apt-get install -y libboost-dev libcurl-dev
+if [[ $(uname) == 'Darwin']]; then
+	brew install boost curl
+else
+	sudo apt-get install -y libboost-dev libcurl-dev
+fi
+
 git clone https://github.com/ABeltramo/otachecker-u
 make -C otachecker-u && cp bin/otachecker /usr/local/bin
 
-wget https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.2.10-Linux.deb
-sudo dpkg -i Hopper-v4-4.2.10-Linux.deb
+if [[ $(uname) == 'Linux' ]]; then
+	wget https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.2.10-Linux.deb
+	sudo dpkg -i Hopper-v4-4.2.10-Linux.deb
+fi
