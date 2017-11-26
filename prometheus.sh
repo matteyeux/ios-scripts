04f308a01b9851441d5a5fcc5672fc96dc1d78a9
matteyeux <mathieu.hautebas@gmail.com>
2017-11-26 23:22:53 +0100

    updated script to support macOS

--- prometheus.sh
+++ prometheus.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+if [ -d "WD" ]; then
+	echo "Removing WD"
+	sudo rm -rf WD
+fi
+
+mkdir WD
+cd WD
+
+
+
+if [[ $(uname) == 'Darwin' ]]; then
+	tool=futurerestore_macos
+elif [[ $(uname) == 'Linux' ]]; then
+	tool=futurerestore_linux
+else
+	echo "$(uname) not supported"
+fi
+
+git clone https://github.com/tihmstar/libfragmentzip && cd libfragmentzip
+./autogen.sh && sudo make install
+cd ..
 
 git clone --recursive https://github.com/tihmstar/tsschecker
 ./tsschecker/autohen.sh
@@ -8,5 +29,5 @@ mkdir tmp && cd tmp
 wget http://api.tihmstar.net/builds/futurerestore/futurerestore-latest.zip
 unzip futurerestore-latest.zip
 
-chmod +x futurerestore_linux && cp futurerestore_linux /usr/local/bin/futurerestore
+chmod +x $tool && sudo cp $tool /usr/local/bin/futurerestore
 cd ../ && rm -rf tmp
